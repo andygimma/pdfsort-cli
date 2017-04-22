8f727f4587efb94c3465234cb4f402c4008235c4
andygimma <andy.gimma@kiron.ngo>
2017-04-22 14:26:45 -0400

    rotate all

--- pdfsort.py
+++ pdfsort.py
@@ -1,15 +1,16 @@
 import click
 
+from rotate_all import rotate_all as rotate_function
+
 @click.group(invoke_without_command=False)
 @click.pass_context
 def cli(ctx):
     click.echo('Thank you for using pdfsort %s' % ctx.invoked_subcommand)
 
 @cli.command()
-@click.argument('name')
-def sync(name):
-    # click.echo('The subcommand')
-    click.echo(name)
-    # click.secho('I am green!', fg='green')
-    # click.secho('Some more text', bg='blue', fg='white')
-    # click.secho('ATTENTION', blink=True, bold=True)
+@click.option('--degrees', default=180, help='degrees to rotate by')
+@click.argument('input_pdf')
+@click.argument('output_pdf')
+def rotate_all(input_pdf, output_pdf, degrees):
+    print input_pdf, output_pdf, degrees
+    rotate_function(input_pdf, output_pdf, degrees)
