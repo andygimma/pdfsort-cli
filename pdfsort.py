74052e99398868e789ead119631c314aac2ceccf
andygimma <andy.gimma@kiron.ngo>
2017-04-22 15:00:43 -0400

    rotate by pages

--- pdfsort.py
+++ pdfsort.py
@@ -3,6 +3,7 @@ import time
 
 from rotate_all import rotate_all as rotate_function
 from generate_new_pdf import generate_new_pdf
+from rotate_pages_by_degrees import rotate_pages_by_degrees
 
 @click.group(invoke_without_command=False)
 @click.pass_context
@@ -31,8 +32,28 @@ def generate(input_pdf, output_pdf, pages_list):
     time1 = time.time()
     description = "Creating " + output_pdf + " from " + input_pdf + " pages " + pages_list
     click.secho(description, fg='green')
+
     generate_new_pdf(input_pdf, pages_list, output_pdf)
+
     click.secho("Created: " + output_pdf, fg='green')
     time2 = time.time()
     time_string = '%0.3f seconds' % ((time2-time1))
+    click.secho(time_string, fg='green')
+
+@cli.command()
+@click.argument('input_pdf')
+@click.argument('output_pdf')
+@click.argument('pages_list')
+@click.argument('degrees')
+def rotate_pages(input_pdf, output_pdf, pages_list, degrees):
+    time1 = time.time()
+    description = "Rotating pages " + pages_list + " " + degrees + " from " + input_pdf + " to " + output_pdf
+    click.secho(description, fg='green')
+
+    rotate_pages_by_degrees(input_pdf, pages_list, output_pdf, int(degrees))
+
+    click.secho("Created: " + output_pdf, fg='green')
+    time2 = time.time()
+    time_string = '%0.3f seconds' % ((time2-time1))
+    time_string = "Finished: " + time_string
     click.secho(time_string, fg='green')
--- rotate_pages_by_degrees.py
+++ rotate_pages_by_degrees.py
@@ -3,20 +3,19 @@ import PyPDF2
 import sys
 from list_numbers import list_numbers
 
-def generate_new_pdf(input_pdf, pages_list, output_pdf):
-    ''' Create a new pdf from a given list of pages '''
+def rotate_pages_by_degrees(pdf, pages_list, output_pdf, clockwise_rotation_in_degrees):
     pages_list = list_numbers(pages_list)
-    pdf_in = open(input_pdf, 'rb')
+    pdf_in = open(pdf, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_in)
     pdf_writer = PyPDF2.PdfFileWriter()
     for index in range(pdf_reader.numPages):
         new_page = pdf_reader.getPage(index)
         if index in pages_list:
             new_page = pdf_reader.getPage(index)
-            string = "added page: " + str(index+1)
-            pdf_writer.addPage(new_page)
+            string = "rotated page: " + str(index + 1) + " " + str(clockwise_rotation_in_degrees) + " degrees"
+            new_page.rotateClockwise(clockwise_rotation_in_degrees)
             click.secho(string, fg='blue')
-
+        pdf_writer.addPage(new_page)
     pdf_out = open(output_pdf, 'wb')
     pdf_writer.write(pdf_out)
     pdf_out.close()
